e7d58329f2ad3d7b979575b58bb9321e2bb88309
Fayshal Rana <imfayshalrana@gmail.com>
2024-11-16 05:03:16 +0600

    oooo

--- resources/views/ui/home/client-section.blade.php
+++ resources/views/ui/home/client-section.blade.php
@@ -1,163 +1,166 @@
 <section class="client-section text-white">
     <div class="client-section-inner">
         <div class="client-left-sec">
-            
-                <ul class="firstcl">
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-1.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
-                        </div>
-                        <p>Podium</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-3.svg')}}">
-                        </div>
-                        <p>Clickup</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-1.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
-                        </div>
-                        <p>Podium</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-3.svg')}}">
-                        </div>
-                        <p>Clickup</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                </ul>
-                <ul class="middlecl">
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-5.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-6.svg')}}">
-                        </div>
-                        <p>Podium</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
-                        </div>
-                        <p>Clickup</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-5.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-6.svg')}}">
-                        </div>
-                        <p>Podium</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
-                        </div>
-                        <p>Clickup</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                </ul>
-                <ul class="lastcl">
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-7.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-8.svg')}}">
-                        </div>
-                        <p>Podium</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-9.svg')}}">
-                        </div>
-                        <p>Clickup</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-7.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-8.svg')}}">
-                        </div>
-                        <p>Podium</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-9.svg')}}">
-                        </div>
-                        <p>Clickup</p>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
-                        </div>
-                        <p>Canvas</p>
-                    </li>
-                </ul>
+
+            <ul class="firstcl">
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-1.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
+                    </div>
+                    <p>Podium</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-3.svg')}}">
+                    </div>
+                    <p>Clickup</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-1.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
+                    </div>
+                    <p>Podium</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-3.svg')}}">
+                    </div>
+                    <p>Clickup</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+            </ul>
+            <ul class="middlecl">
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-5.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-6.svg')}}">
+                    </div>
+                    <p>Podium</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
+                    </div>
+                    <p>Clickup</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-5.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-6.svg')}}">
+                    </div>
+                    <p>Podium</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-2.svg')}}">
+                    </div>
+                    <p>Clickup</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+            </ul>
+            <ul class="lastcl">
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-7.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-8.svg')}}">
+                    </div>
+                    <p>Podium</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-9.svg')}}">
+                    </div>
+                    <p>Clickup</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-7.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-8.svg')}}">
+                    </div>
+                    <p>Podium</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-9.svg')}}">
+                    </div>
+                    <p>Clickup</p>
+                </li>
+                <li>
+                    <div class="icon">
+                        <img src="{{URL::asset('assets/img/clogo-4.svg')}}">
+                    </div>
+                    <p>Canvas</p>
+                </li>
+            </ul>
         </div>
         <div class="client-right-sec fade-in-from-top">
-                <h2 class="section-title text-white">Integrated with <br><span>best apps.</span></h3>
-                <p>Experience effortless workflow integration as the best apps seamlessly connect with our productivity calendar. Tasks, meetings, and productivity are managed efficiently, maximizing your potential with superior integrations.</p>
+            <h2 class="section-title text-white">Integrated with <br><span>best apps.</span></h3>
+                <p>Experience effortless workflow integration as the best apps seamlessly connect with our productivity
+                    calendar. Tasks, meetings, and productivity are managed efficiently, maximizing your potential with
+                    superior integrations.</p>
                 <div class="">
-                    <a href="#" class="client-btn">Learn More <img src="http://127.0.0.1:8000/assets/img/arrow-icon.svg"></a>
+                    <a href="#" class="client-btn">Learn More <img
+                            src="{{URL::asset('assets/img/arrow-icon.svg')}}"></a>
                 </div>
         </div>
         <div class="client-overlay"></div>
