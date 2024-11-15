2e6468c96659873c49c7d7e9608b7a1131803515
Fayshal Rana <imfayshalrana@gmail.com>
2024-11-16 02:08:16 +0600

    fixed on controller

--- app/Http/Controllers/UiController.php
+++ app/Http/Controllers/UiController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Routing\Controller as BaseController;
 
-class UiController extends Controller
+class Controller extends BaseController
 {
     public function welcomePage() {
         return view('welcome');
