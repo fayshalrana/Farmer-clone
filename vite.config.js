53ace6be1fcccae72aded74a60880d5b815dbf89
Fayshal Rana <imfayshalrana@gmail.com>
2024-11-16 02:27:13 +0600

    add laravel-vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist'
-  }
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js', 'resources/css/app.css'],
+      refresh: true,
+    }),
+  ],
 });
